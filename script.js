a87e7263427d7539937ccb6c635d1128a30309e8
Aditi Chaubey <aditichaubey8@gmail.com>
2026-07-10 19:10:21 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var typed = new Typed(".typing", {
     strings: [
-    "Web Developer",
-    "Python Programmer",
-    "B.Tech CSE Student"
-],
-    typeSpeed: 70,
-    backSpeed: 50,
+        "B.Tech CSE Student",
+        "Web Developer",
+        "Python Programmer",
+        "Future Google Engineer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
     loop: true
 });
