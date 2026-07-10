adb2c19eaf07ec512ecee89d8ce83a23b863f110
Aditi Chaubey <aditichaubey8@gmail.com>
2026-07-10 14:55:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings: [
+    "Web Developer",
+    "Python Programmer",
+    "B.Tech CSE Student"
+],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
